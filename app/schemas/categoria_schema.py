9227865c5d6311db6e6cccad93ef3628612155e2
HenryGranda <huambi123@gmail.com>
2025-06-02 02:04:03 -0500

    Creación de la clase categoria_schema.py

---
+++ app/schemas/categoria_schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class CategoriaCreate(BaseModel):
+    nombre: str
+
+class CategoriaOut(CategoriaCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
