ea36affbdc873d1e597714be2ef7dd32bd47e652
Abenavidese <abenavidese@est.ups.edu.ec>
2025-06-04 00:40:48 -0500

    Feat: Cliente logica agregada

--- app/controllers/cliente_controller.py
+++ app/controllers/cliente_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.config.database import SessionLocal
 from app.schemas.cliente_schema import ClienteCreate, ClienteOut
+from app.services.cliente_service import crear_cliente_service, listar_clientes_service
 
 router = APIRouter(prefix="/clientes", tags=["Clientes"])
 
@@ -14,12 +15,8 @@ def get_db():
 
 @router.post("/", response_model=ClienteOut)
 def crear_cliente(cliente: ClienteCreate, db: Session = Depends(get_db)):
-    nuevo_cliente = Cliente(**cliente.dict())
-    db.add(nuevo_cliente)
-    db.commit()
-    db.refresh(nuevo_cliente)
-    return nuevo_cliente
+    return crear_cliente_service(cliente, db)
 
 @router.get("/", response_model=list[ClienteOut])
 def listar_clientes(db: Session = Depends(get_db)):
-    return db.query(Cliente).all()
+    return listar_clientes_service(db)
