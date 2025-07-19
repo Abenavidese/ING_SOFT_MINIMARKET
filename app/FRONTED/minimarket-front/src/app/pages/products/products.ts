1d1e2f1c8a98ec5dc8e2e1d9db0463cdaccb75e2
Abenavidese <abenavidese@est.ups.edu.ec>
2025-07-19 18:13:36 -0500

    Feat: Agregada logica de productos al frontend

--- app/FRONTED/minimarket-front/src/app/pages/products/products.ts
+++ app/FRONTED/minimarket-front/src/app/pages/products/products.ts
@@ -2,27 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Producto, ProductService } from '../../services/product-service';
+import { Proveedor, ProveedorService } from '../../services/proveedor-service';
+import { Categoria, CategoriaService } from '../../services/categoria-service';
+
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './products.html',
-  styleUrl: './products.scss'
+  styleUrls: ['./products.scss']
 })
 export class Products implements OnInit {
   productos: Producto[] = [];
-  nuevoProducto: Producto = { nombre: '', precio: 0, stock: 0, proveedor_id: 1, categoria_id: 1 };
+  proveedores: Proveedor[] = [];
+  categorias: Categoria[] = [];
 
-  constructor(private productService: ProductService) {}
+  nuevoProducto: Producto = {
+    nombre: '',
+    precio: 0,
+    stock: 0,
+    proveedor_id: 1,
+    categoria_id: 1
+  };
+
+  constructor(
+    private productService: ProductService,
+    private proveedorService: ProveedorService,
+    private categoriaService: CategoriaService
+  ) {}
 
   ngOnInit() {
     this.cargarProductos();
+    this.cargarProveedores();
+    this.cargarCategorias();
   }
 
   cargarProductos() {
     this.productService.listar().subscribe({
-      next: (data) => this.productos = data,
+      next: (data) => (this.productos = data),
       error: (err) => console.error('Error al listar productos:', err)
+    });
+  }
+
+  cargarProveedores() {
+    this.proveedorService.listar().subscribe({
+      next: (data) => (this.proveedores = data),
+      error: (err) => console.error('Error al listar proveedores:', err)
+    });
+  }
+
+  cargarCategorias() {
+    this.categoriaService.listar().subscribe({
+      next: (data) => (this.categorias = data),
+      error: (err) => console.error('Error al listar categorías:', err)
     });
   }
 
