0eab937dc98e84b707e0f17335318a432fbe695b
Abenavidese <abenavidese@est.ups.edu.ec>
2025-07-19 18:17:18 -0500

    Feat: Products.ts mejorado

--- app/FRONTED/minimarket-front/src/app/pages/products/products.ts
+++ app/FRONTED/minimarket-front/src/app/pages/products/products.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Producto, ProductService } from '../../services/product-service';
@@ -21,14 +21,15 @@ export class Products implements OnInit {
     nombre: '',
     precio: 0,
     stock: 0,
-    proveedor_id: 1,
-    categoria_id: 1
+    proveedor_id: 0,
+    categoria_id: 0
   };
 
   constructor(
     private productService: ProductService,
     private proveedorService: ProveedorService,
-    private categoriaService: CategoriaService
+    private categoriaService: CategoriaService,
+    private cdr: ChangeDetectorRef // <-- Inyectamos ChangeDetectorRef
   ) {}
 
   ngOnInit() {
@@ -39,21 +40,36 @@ export class Products implements OnInit {
 
   cargarProductos() {
     this.productService.listar().subscribe({
-      next: (data) => (this.productos = data),
+      next: (data) => {
+        this.productos = data;
+        this.cdr.detectChanges(); // <-- Forzamos actualización de la vista
+      },
       error: (err) => console.error('Error al listar productos:', err)
     });
   }
 
   cargarProveedores() {
     this.proveedorService.listar().subscribe({
-      next: (data) => (this.proveedores = data),
+      next: (data) => {
+        this.proveedores = data;
+        if (this.proveedores.length > 0) {
+          this.nuevoProducto.proveedor_id = this.proveedores[0].id!;
+        }
+        this.cdr.detectChanges();
+      },
       error: (err) => console.error('Error al listar proveedores:', err)
     });
   }
 
   cargarCategorias() {
     this.categoriaService.listar().subscribe({
-      next: (data) => (this.categorias = data),
+      next: (data) => {
+        this.categorias = data;
+        if (this.categorias.length > 0) {
+          this.nuevoProducto.categoria_id = this.categorias[0].id!;
+        }
+        this.cdr.detectChanges();
+      },
       error: (err) => console.error('Error al listar categorías:', err)
     });
   }
@@ -61,7 +77,13 @@ export class Products implements OnInit {
   crearProducto() {
     this.productService.crear(this.nuevoProducto).subscribe({
       next: () => {
-        this.nuevoProducto = { nombre: '', precio: 0, stock: 0, proveedor_id: 1, categoria_id: 1 };
+        this.nuevoProducto = {
+          nombre: '',
+          precio: 0,
+          stock: 0,
+          proveedor_id: this.proveedores.length > 0 ? this.proveedores[0].id! : 0,
+          categoria_id: this.categorias.length > 0 ? this.categorias[0].id! : 0
+        };
         this.cargarProductos();
       },
       error: (err) => console.error('Error al crear producto:', err)
