d76cb65c8f7207899660aa52ce890c7792c0b917
BRYAMMMMM <bperaltan@est.ups.edu.ec>
2025-07-22 14:46:18 -0500

    Mejora menu

--- app/FRONTED/minimarket-front/src/app/app.ts
+++ app/FRONTED/minimarket-front/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Menu } from './pages/menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu], 
+  imports: [CommonModule, RouterOutlet, Menu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('minimarket-front');
+
+  constructor(private router: Router) {}
+
+  get isAuthenticated(): boolean {
+    return this.router.url !== '/login';
+  }
 }
